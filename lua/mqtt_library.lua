c851a603ee71b469e09979f483572b65bf51b577
Andy Gelme <andyg@geekscape.org>
2012-05-17 07:49:22 +1000

    For Lua on OpenWRT, change all string.char(N) to be string.char(math.floor(N)), whenever it is possible that "N" is number and not an integer.

--- lua/mqtt_library.lua
+++ lua/mqtt_library.lua
@@ -205,7 +205,7 @@ function MQTT.client:connect(                                     -- Public API
 
 -- Keep alive timer (bytes 11 LSB and 12 MSB, unit is seconds)
 -- ~~~~~~~~~~~~~~~~~
-  payload = payload .. string.char(MQTT.client.KEEP_ALIVE_TIME / 256)
+  payload = payload .. string.char(math.floor(MQTT.client.KEEP_ALIVE_TIME / 256))
   payload = payload .. string.char(MQTT.client.KEEP_ALIVE_TIME % 256)
 
 -- Client identifier
@@ -273,7 +273,7 @@ function MQTT.client.encode_utf8(                               -- Internal API
   input)  -- string
 
   local output
-  output = string.char(#input / 256)
+  output = string.char(math.floor(#input / 256))
   output = output .. string.char(#input % 256)
   output = output .. input
 
@@ -703,7 +703,7 @@ function MQTT.client:subscribe(                                   -- Public API
   self.message_id = self.message_id + 1
 
   local message
-  message = string.char(self.message_id / 256)
+  message = string.char(math.floor(self.message_id / 256))
   message = message .. string.char(self.message_id % 256)
 
   for index, topic in ipairs(topics) do
@@ -738,7 +738,7 @@ function MQTT.client:unsubscribe(                                 -- Public API
   self.message_id = self.message_id + 1
 
   local message
-  message = string.char(self.message_id / 256)
+  message = string.char(math.floor(self.message_id / 256))
   message = message .. string.char(self.message_id % 256)
 
   for index, topic in ipairs(topics) do
